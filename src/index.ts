2994e469dab92bcfb7358011cc2a2ffcfb1536cb
Mircea Nistor <mirceanis@gmail.com>
2019-10-29 17:31:51 +0100

    style: run formatter

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@
  * represents the result of a status check
  */
 export interface CredentialStatus {
-    [x: string]: any
+  [x: string]: any
 }
 
 export interface StatusMethod {
-    checkStatus(credential : string) : CredentialStatus
+  checkStatus(credential: string): CredentialStatus
 }
 
 export class Status implements StatusMethod {
-    checkStatus(credential: string) : CredentialStatus {
-        return {revoked : false}
-    }
+  checkStatus(credential: string): CredentialStatus {
+    return { revoked: false }
+  }
 }
