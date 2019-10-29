342f4bbcdfb6eb601de284a4aa934f6ba0736e8d
Mircea Nistor <mirceanis@gmail.com>
2019-10-29 14:27:24 +0100

    chore: extract status from credential

--- src/__tests__/basicTests.ts
+++ src/__tests__/basicTests.ts
@@ -1,5 +1,20 @@
+import 'jest-extended'
+
 import { Status } from '../index'
 
-test('should pass this test', () => {
-  expect(false).toBe(false)
+test('should be able to instantiate Status', () => {
+  expect(new Status()).not.toBeNil()
+})
+
+test('should be able to call checkStatus', () => {
+  const checker = new Status()
+  expect(checker.checkStatus).toBeFunction()
+})
+
+test('should be able to extract status entry from a credential', async () => {
+  const checker = new Status()
+  //does not return the status result, only forwards the input at this time
+  const statusEntry = await checker.checkStatus("eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NzIzNTExNTYsInN0YXR1cyI6eyJpZCI6InJpbmtlYnk6MHhTdGF0dXNSZWdpc3RyeUFkZHJlc3MiLCJ0eXBlIjoiRXRoclN0YXR1c1JlZ2lzdHJ5MjAxOSJ9LCJpc3MiOiJkaWQ6ZXRocjoweGYzYmVhYzMwYzQ5OGQ5ZTI2ODY1ZjM0ZmNhYTU3ZGJiOTM1YjBkNzQifQ.hNFm3JDwoyNX2YHPlioHkR986ETPPWYuBV_9J9HYVaGKUVeKCrrO3a5ZExP3WVPv21P7NlpHeYMipjKQF_G1lAE")
+  expect(statusEntry).toStrictEqual({type: "EthrStatusRegistry2019", id: "rinkeby:0xStatusRegistryAddress"})
+  console.log(statusEntry)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
+import { decodeJWT } from 'did-jwt'
 /**
  * represents the result of a status check
  */
 export interface CredentialStatus {
+  revoked: boolean | null
   [x: string]: any
 }
 
 export interface StatusMethod {
-  checkStatus(credential: string): CredentialStatus
+  checkStatus(credential : string) : Promise<CredentialStatus>
 }
 
 export class Status implements StatusMethod {
-  checkStatus(credential: string): CredentialStatus {
-    return { revoked: false }
+  checkStatus(credential: string) : Promise<CredentialStatus> {
+    //TODO: validate the credential to be VerifiableCredential or VerifiablePresentation
+    //~~TODO: extract the status property~~
+    const decoded = decodeJWT(credential)
+    const statusEntry = (decoded.payload as any).status
+    //TODO: get the type of the status property
+
+    //TODO: call the statusMethod type from the statusMethodRegistry
+    //TODO: return the result of the call or an error? if the method was not registered
+
+
+    return new Promise((resolve, reject) => { resolve( statusEntry ) })
   }
 }
