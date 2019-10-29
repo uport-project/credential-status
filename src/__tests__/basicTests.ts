bb8a3db48872048d88c87e1d6c7fc97f32c984eb
Mircea Nistor <mirceanis@gmail.com>
2019-10-29 16:41:47 +0100

    feat: implement basic status registry

--- src/__tests__/basicTests.ts
+++ src/__tests__/basicTests.ts
@@ -1,6 +1,6 @@
 import 'jest-extended'
 
-import { Status } from '../index'
+import { Status, StatusMethod, CredentialStatus } from '../index'
 
 test('should be able to instantiate Status', () => {
   expect(new Status()).not.toBeNil()
@@ -11,10 +11,40 @@ test('should be able to call checkStatus', () => {
   expect(checker.checkStatus).toBeFunction()
 })
 
-test('should be able to extract status entry from a credential', async () => {
+test('should reject unknown status method', async () => {
   const checker = new Status()
   //does not return the status result, only forwards the input at this time
-  const statusEntry = await checker.checkStatus("eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NzIzNTExNTYsInN0YXR1cyI6eyJpZCI6InJpbmtlYnk6MHhTdGF0dXNSZWdpc3RyeUFkZHJlc3MiLCJ0eXBlIjoiRXRoclN0YXR1c1JlZ2lzdHJ5MjAxOSJ9LCJpc3MiOiJkaWQ6ZXRocjoweGYzYmVhYzMwYzQ5OGQ5ZTI2ODY1ZjM0ZmNhYTU3ZGJiOTM1YjBkNzQifQ.hNFm3JDwoyNX2YHPlioHkR986ETPPWYuBV_9J9HYVaGKUVeKCrrO3a5ZExP3WVPv21P7NlpHeYMipjKQF_G1lAE")
-  expect(statusEntry).toStrictEqual({type: "EthrStatusRegistry2019", id: "rinkeby:0xStatusRegistryAddress"})
-  console.log(statusEntry)
+  const statusEntry = await checker.checkStatus(
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NzIzNTExNTYsInN0YXR1cyI6eyJpZCI6InJpbmtlYnk6MHhTdGF0dXNSZWdpc3RyeUFkZHJlc3MiLCJ0eXBlIjoiRXRoclN0YXR1c1JlZ2lzdHJ5MjAxOSJ9LCJpc3MiOiJkaWQ6ZXRocjoweGYzYmVhYzMwYzQ5OGQ5ZTI2ODY1ZjM0ZmNhYTU3ZGJiOTM1YjBkNzQifQ.hNFm3JDwoyNX2YHPlioHkR986ETPPWYuBV_9J9HYVaGKUVeKCrrO3a5ZExP3WVPv21P7NlpHeYMipjKQF_G1lAE'
+  )
+  expect(statusEntry).toStrictEqual({
+    error:
+      'Credential status method EthrStatusRegistry2019 unknown. Validity can not be determined.'
+  })
+})
+
+test('should pass through credential with no status requirement', async () => {
+  const checker = new Status()
+  //does not return the status result, only forwards the input at this time
+  const statusEntry = await checker.checkStatus(
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NzIzNjI4NzUsIm5hbWUiOiJ1UG9ydCBEZXZlbG9wZXIiLCJpc3MiOiJkaWQ6ZXRocjoweGYzYmVhYzMwYzQ5OGQ5ZTI2ODY1ZjM0ZmNhYTU3ZGJiOTM1YjBkNzQifQ.dTkC_-pLizlz2LkwZBIVPLiFJBGI51urLB2sPgivemf78u3RpB2gJG7Xd4BGUFA3kjOoujAMFNKX4MakoskU3gA'
+  )
+  expect(statusEntry).toStrictEqual({})
+})
+
+test('should check status according to registered method', async () => {
+  const customMethod = <StatusMethod>{
+    checkStatus(credential: string): Promise<CredentialStatus> {
+      return new Promise((resolve, reject) => {
+        resolve({ 'custom method works': true })
+      })
+    }
+  }
+
+  const checker = new Status({ CustomStatusChecker: customMethod })
+  //does not return the status result, only forwards the input at this time
+  const statusEntry = await checker.checkStatus(
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NzIzNjM1NjQsInN0YXR1cyI6eyJ0eXBlIjoiQ3VzdG9tU3RhdHVzQ2hlY2tlciIsImlkIjoiZG9uJ3QgY2FyZSJ9LCJpc3MiOiJkaWQ6ZXRocjoweGYzYmVhYzMwYzQ5OGQ5ZTI2ODY1ZjM0ZmNhYTU3ZGJiOTM1YjBkNzQifQ.zJmp94s0yq7HYHhVT1A0GFD4-51vwpsc7JPn35mMgZJ7WVVQmlp07u13Uu0vqsFprixHL3zRfd9z-yWKUMTyWQA'
+  )
+  expect(statusEntry).toStrictEqual({ 'custom method works': true })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,77 @@
 import { decodeJWT } from 'did-jwt'
+
 /**
- * represents the result of a status check
+ * Represents the result of a status check
  */
 export interface CredentialStatus {
-  revoked: boolean | null
+  revoked?: boolean
   [x: string]: any
 }
 
-export interface StatusMethod {
-  checkStatus(credential : string) : Promise<CredentialStatus>
+/**
+ * Represents a status method entry that could be embedded in a W3C Verifiable Credential.
+ * Normally, only credentials that list a status method would need to be verified by it.
+ *
+ * ex:
+ * ```json
+ * status : { type: "EthrStatusRegistry2019", id: "rinkeby:0xregistryAddress" }
+ * ```
+ *
+ */
+export interface StatusEntry {
+  type: string
+  id: string
+  [x: string]: any
 }
 
+/**
+ * [draft] The interface used by status methods.
+ * `checkStatus` should be called with a raw credential and it should produce a status result.
+ */
+export interface StatusMethod {
+  checkStatus(credential: string): Promise<CredentialStatus>
+}
+
+interface JWTPayloadWithStatus {
+  status?: StatusEntry
+  [x: string]: any
+}
+
+interface StatusMethodRegistry {
+  [type: string]: StatusMethod
+}
+
+/**
+ * [draft] An implementation of a StatusMethod that can aggregate multiple other methods.
+ * It calls the appropriate method based on the `status.type` specified in the credential.
+ */
 export class Status implements StatusMethod {
-  checkStatus(credential: string) : Promise<CredentialStatus> {
-    //TODO: validate the credential to be VerifiableCredential or VerifiablePresentation
-    //~~TODO: extract the status property~~
+  private registry: StatusMethodRegistry
+  constructor(registry: StatusMethodRegistry = {}) {
+    this.registry = registry
+  }
+
+  checkStatus(credential: string): Promise<CredentialStatus> {
+    // TODO: validate the credential to be VerifiableCredential or VerifiablePresentation
     const decoded = decodeJWT(credential)
-    const statusEntry = (decoded.payload as any).status
-    //TODO: get the type of the status property
+    const statusEntry = (decoded.payload as JWTPayloadWithStatus).status
 
-    //TODO: call the statusMethod type from the statusMethodRegistry
-    //TODO: return the result of the call or an error? if the method was not registered
+    if (typeof statusEntry === 'undefined') {
+      return new Promise((resolve, reject) => {
+        resolve({})
+      })
+    }
 
+    const method = this.registry[statusEntry.type]
 
-    return new Promise((resolve, reject) => { resolve( statusEntry ) })
+    if (typeof method !== 'undefined' && method != null) {
+      return method.checkStatus(credential)
+    } else {
+      return new Promise((resolve, reject) => {
+        resolve({
+          error: `Credential status method ${statusEntry.type} unknown. Validity can not be determined.`
+        })
+      })
+    }
   }
 }
