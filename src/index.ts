52f68fa84f53a53b2f23ae1d628f6c727a56cbfe
Mircea Nistor <mirceanis@gmail.com>
2022-02-16 16:44:00 +0100

    feat: add support for more credential formats (#39)
    fixes #38
    
    BREAKING CHANGE: The method signature for `StatusMethod` requires support for both credential formats
    BREAKING CHANGE: The return type of `StatusMethod`  is `Promise<CredentialStatus>` with the expectation to reject if there is a problem resolving the status.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import { decodeJWT } from 'did-jwt'
 import { DIDDocument } from 'did-resolver'
 
 /**
- * Represents the result of a status check
+ * Represents the result of a status check.
+ *
+ * Implementations should populate the `revoked` boolean property, but they can return additional metadata that is
+ * method specific.
+ *
+ * @alpha This API is still being developed and may be updated. Please follow progress or suggest improvements at
+ *   [https://github.com/uport-project/credential-status]
  */
 export interface CredentialStatus {
   revoked?: boolean
@@ -17,8 +23,15 @@ export interface CredentialStatus {
  *
  * ex:
  * ```json
- * status : { type: "EthrStatusRegistry2019", id: "rinkeby:0xregistryAddress" }
+ * credentialStatus: {
+ *   type: "EthrStatusRegistry2019",
+ *   id: "rinkeby:0xregistryAddress"
+ * }
  * ```
+ * See https://www.w3.org/TR/vc-data-model/#status
+ *
+ * @alpha This API is still being developed and may be updated. Please follow progress or suggest improvements at
+ *   [https://github.com/uport-project/credential-status]
  */
 export interface StatusEntry {
   type: string
@@ -29,7 +42,7 @@ export interface StatusEntry {
 }
 
 /**
- * [draft] The interface expected for status resolvers.
+ * The interface expected for status resolvers.
  * `checkStatus` should be called with a raw credential and it should Promise a [[CredentialStatus]] result.
  * It is advisable that classes that implement this interface also provide a way to easily register the correct
  * Status method type.
@@ -43,33 +56,44 @@ export interface StatusEntry {
  *    asStatusMethod = {"CredentialStatusList2017" : this.checkStatus}
  *  }
  * ```
+ *
+ * @alpha This API is still being developed and may be updated. Please follow progress or suggest improvements at
+ *   [https://github.com/uport-project/credential-status]
  */
 export interface StatusResolver {
   checkStatus: StatusMethod
 }
 
 /**
- * The method signature expected to be implemented by credential status resolvers
+ * The Verifiable Credential or Presentation to be verified in either JSON/JSON-LD or JWT format.
+ *
+ * @alpha This API is still being developed and may be updated. Please follow progress or suggest improvements at
+ *   [https://github.com/uport-project/credential-status]
  */
-export type StatusMethod = (credential: string, didDoc: DIDDocument) => Promise<null | CredentialStatus>
+export type CredentialJwtOrJSON = string | { credentialStatus?: StatusEntry }
 
-interface JWTPayloadWithStatus {
-  credentialStatus?: StatusEntry
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [x: string]: any
-}
-
-interface StatusMethodRegistry {
-  [type: string]: StatusMethod
-}
+/**
+ * The method signature expected to be implemented by credential status resolvers.
+ *
+ * @param credential The credential whose status will be verified
+ * @param didDoc The DID document of the issuer.
+ *
+ * @return a Promise resolving to a `CredentialStatus` object or rejecting with a reason.
+ *
+ * @alpha This API is still being developed and may be updated. Please follow progress or suggest improvements at
+ *   [https://github.com/uport-project/credential-status]
+ */
+export type StatusMethod = (credential: CredentialJwtOrJSON, didDoc: DIDDocument) => Promise<CredentialStatus>
 
 /**
  * [draft] An implementation of a StatusMethod that can aggregate multiple other methods.
- * It calls the appropriate method based on the `status.type` specified in the credential.
+ * It calls the appropriate method based on the `credentialStatus.type` specified in the credential.
+ *
+ * @alpha This API is still being developed and may be updated. Please follow progress or suggest improvements at
+ *   [https://github.com/uport-project/credential-status]
  */
 export class Status implements StatusResolver {
-  private registry: StatusMethodRegistry
+  private registry: Record<string, StatusMethod>
 
   /**
    * All the expected StatusMethods should be registered during construction.
@@ -84,28 +108,50 @@ export class Status implements StatusResolver {
    * })
    * ```
    */
-  constructor(registry: StatusMethodRegistry = {}) {
+  constructor(registry: Record<string, StatusMethod> = {}) {
     this.registry = registry
   }
 
-  async checkStatus(credential: string, didDoc: DIDDocument): Promise<null | CredentialStatus> {
-    // TODO: validate the credential to be VerifiableCredential or VerifiablePresentation
-    const decoded = decodeJWT(credential)
-    const statusEntry = (decoded.payload as JWTPayloadWithStatus).credentialStatus
+  async checkStatus(credential: CredentialJwtOrJSON, didDoc: DIDDocument): Promise<CredentialStatus> {
+    let statusEntry: StatusEntry | undefined = undefined
 
-    if (typeof statusEntry === 'undefined') {
-      return {}
+    if (typeof credential === 'string') {
+      try {
+        const decoded = decodeJWT(credential)
+        statusEntry =
+          decoded?.payload?.vc?.credentialStatus || // JWT Verifiable Credential payload
+          decoded?.payload?.vp?.credentialStatus || // JWT Verifiable Presentation payload
+          decoded?.payload?.credentialStatus // legacy JWT payload
+      } catch (e1: unknown) {
+        // not a JWT credential or presentation
+        try {
+          const decoded = JSON.parse(credential)
+          statusEntry = decoded?.credentialStatus
+        } catch (e2: unknown) {
+          // not a JSON either.
+        }
+      }
+    } else {
+      statusEntry = credential.credentialStatus
+    }
+
+    if (!statusEntry) {
+      return {
+        revoked: false,
+        message: 'credentialStatus property was not set on the original credential',
+      }
+    } else if (typeof statusEntry !== 'object' || !statusEntry?.type) {
+      throw new Error('bad_request: credentialStatus entry is not formatted correctly. Validity can not be determined.')
     }
 
     const method = this.registry[statusEntry.type]
 
-    if (typeof method !== 'undefined' && method != null) {
-      return method(credential, didDoc)
+    if (!method) {
+      throw new Error(
+        `unknown_method: credentialStatus method ${statusEntry.type} unknown. Validity can not be determined.`
+      )
     } else {
-      return {
-        // Once the credential status mechanisms in W3C get more stable, perhaps this can become a `reject`
-        error: `Credential status method ${statusEntry.type} unknown. Validity can not be determined.`,
-      }
+      return method(credential, didDoc)
     }
   }
 }
